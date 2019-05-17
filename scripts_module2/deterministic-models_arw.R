5600ab9e108b336d9c502354f255096a7a7fbb50
Anna Willoughby <16709317+arw36@users.noreply.github.com>
2019-05-17 15:00:00 -0400

    Update deterministic-models_arw.R

--- scripts_module2/deterministic-models_arw.R
+++ scripts_module2/deterministic-models_arw.R
@@ -1,15 +1,9 @@
 ### R code from vignette source 'deterministic-models.rnw'
-### Encoding: UTF-8
+### Anna Willoughby 
+### May 16, 2019
 
-###################################################
-### code chunk number 1: deterministic-models.rnw:104-105
-###################################################
 require(deSolve)                          #deSolve library needed for this computing session
 
-
-###################################################
-### code chunk number 2: deterministic-models.rnw:109-124
-###################################################
 sir.model.closed <- function (t, x, params) {    #here we begin a function with three arguments
   S <- x[1]                               #create local variable S, the first element of x
   I <- x[2]                               #create local variable I
@@ -26,24 +20,12 @@ sir.model.closed <- function (t, x, params) {    #here we begin a function with 
        )
 }
 
-
-###################################################
-### code chunk number 3: deterministic-models.rnw:133-136
-###################################################
 times <- seq(0,120,by=5)                    #function seq returns a sequence
 params <- c(beta=0.3,gamma=1/7)             #function "c" combines values into a vector. these are per day 
 xstart <- c(S=9999/10000,I=1/10000,R=0)     #initial conditions
 
-
-###################################################
-### code chunk number 4: deterministic-models.rnw:140-141
-###################################################
 out <- as.data.frame(ode(xstart,times,sir.model.closed,params))  #result stored in dataframe
 
-
-###################################################
-### code chunk number 5: deterministic-models.rnw:144-149
-###################################################
 op <- par(fig=c(0,0.5,0,1),mar=c(4,4,1,1))                  #set graphical parameters
 plot(I~time,data=out,type='b')                              #plot the I variable against time
 par(fig=c(0.5,1,0,1),mar=c(4,1,1,1),new=T)                  #re-set graphical parameters
@@ -52,7 +34,6 @@ par(op)                                                     #re-set graphical pa
 
 # Exercise 1. Explore the dynamics of the system for different values of the 
 # beta and lambda parameters by simulating and plotting trajectories as time series and in phase space (e.g., I vs. S). 
-
 
 # created a function that produces plot based on beta and gam
 plot_I <- function(b, g){
@@ -74,7 +55,6 @@ library(tidyverse)
 I.plot <- purrr::map2(b_seq, g_seq, plot_I)
 
 # Explore with ggplot 
-
 ggplot(out, aes(x = time,y = I)) + geom_line(colour = viridis(params[[1]])) 
 
 
@@ -135,7 +115,6 @@ plot.bd <- function(b, d){
 plot.bd(0.02, 0.015)
 
 # Exercise 4. Modify the codes given to study a SEIR model 
-
 seir.model.closed <- function (t, x, params) {    #here we begin a function with three arguments
   S <- x[1]                               #create local variable S, the first element of x
   E <- x[2]                               #create local variable E
@@ -153,7 +132,3 @@ seir.model.closed <- function (t, x, params) {    #here we begin a function with
        }
        )
 }
-
-
-
-
